7958499baf11cb8f6547c0704543f442f1f70668
Andrea Llovera De Sousa <124921019+andreasop01@users.noreply.github.com>
2023-12-17 23:39:19 +0100

    comit

--- Examen2019/src/main/java/principal/Principal.java
+++ Examen2019/src/main/java/principal/Principal.java
@@ -6,6 +6,8 @@ import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.List;
+import java.util.Set;
+import java.util.concurrent.TimeUnit;
 import java.util.logging.LogManager;
 import java.util.logging.Logger;
 
@@ -26,9 +28,69 @@ public class Principal {
 		globalLogger.setLevel(java.util.logging.Level.OFF);
 		sesion=Conexion.getSession();
 		
-		ejercicio2("1010",BigInteger.valueOf(1),BigInteger.valueOf(1),"10/12/2023","14/12/2023");
+		//ejercicio2("1010",BigInteger.valueOf(1),BigInteger.valueOf(1),"10/12/2023","14/12/2023");
+		ejercicio3();
+		
 		sesion.close();
 
+	}
+	private static void ejercicio3() {
+		
+		Session session = sesion.openSession();
+		Query cons=session.createQuery("from T3clientes",T3clientes.class);
+		
+		List<T3clientes> listaCli=cons.list();
+		for (T3clientes cli : listaCli) {
+			System.out.println("Cod Cli: "+cli.getCodigocliente()+" Nombre: "+cli.getNombrecliente()+" Apellido: "+cli.getApellido());
+			if(cli.getT3reservases().size()==0) {
+				System.out.println("El cliente no tiene reservas");
+				continue;
+			}
+			System.out.printf("%6s %8s %7s %12s %11s %10s %4s %6s %7s %9s %10s%n","CODRES","NUMHABIT","CAMSUPL","FECHAENTRADA","FECHASALIDA","TOTALSUPLE","DIAS","PVPDIA","IMPORTE","IMPORDESC","IMPORTOTAL\r\n");
+			System.out.printf("%6s %8s %7s %12s %11s %10s %4s %6s %7s %9s %10s%n", "------", "--------", "-------", "------------", "-----------", "----------", "----", "------", "-------", "---------", "----------");
+			
+			int totalSup=0;
+			int totalDias=0;
+			double totalPvd=0;
+			int totalImport=0;
+			int totalImportDesc=0;
+			int totalT=0;
+			double importMax=0;
+			String nResmax="";
+			
+			Set<T3reservas> listRes=cli.getT3reservases();
+			for (T3reservas res : listRes ) {
+				
+				long difMS = res.getFechasalida().getTime() - res.getFechaentrada().getTime();
+				long difDias = TimeUnit.DAYS.convert(difMS, TimeUnit.MILLISECONDS);
+				double importe=(res.getT3habitaciones().getT3tiposhabitaciones().getPrecio()+res.getCamassupletorias().intValue()*10)*difDias;
+				double desc=importe*(res.getDescuento().intValue()/100);
+				totalSup+=res.getCamassupletorias().intValue()*10;
+				totalDias+=difDias;
+				totalPvd+=res.getT3habitaciones().getT3tiposhabitaciones().getPrecio();
+				totalImport+=importe;
+				totalImportDesc+=desc;
+				totalT+=(importe-desc);
+				
+				if(importMax==importe-desc) {
+					nResmax+=res.getCodreserva()+". ";
+				}
+				if(importe-desc>importMax) {
+					nResmax=res.getCodreserva()+". ";
+					importMax=importe-desc;
+				}
+				
+				System.out.printf("%6s %8s %7s %12s %11s %10s %4s %6s %7s %9s %10s%n",res.getCodreserva(),res.getT3habitaciones().getNumhabitacion(),res.getCamassupletorias(),res.getFechaentrada().toString(),res.getFechasalida().toString(),(res.getCamassupletorias().intValue()*10),
+						difDias,res.getT3habitaciones().getT3tiposhabitaciones().getPrecio(),importe,desc,(importe-desc));
+			}
+			
+			System.out.printf("%6s %8s %7s %12s %11s %10s %4s %6s %7s %9s %10s%n", "------", "--------", "-------", "------------", "-----------", "----------", "----", "------", "-------", "---------", "----------");
+			System.out.printf("%9s %5s %7s %12s %11s %10s %4s %6s %7s %9s %10s%n", "TOTALES: ", "", "", "", "", totalSup,totalDias,totalPvd,totalImport,totalImportDesc,totalT);
+			System.out.println("Número de reserva con más importe total (si hay varias visualizarlas): "+nResmax);
+			
+		
+		}
+		
 	}
 	private static void ejercicio2(String numHab, BigInteger codCli, BigInteger suple, String fechaEntrada,
 			String fechaSalida) {
@@ -43,7 +105,7 @@ public class Principal {
 		if(c==null) {
 			System.out.println("Codigo de cliente no existe");
 			return;
-		}
+		} 
 		
 		SimpleDateFormat formato = new SimpleDateFormat("dd-MM-yyyy");
 		formato.setLenient(false);
@@ -90,10 +152,19 @@ public class Principal {
 		}
 		
 		Transaction tx = session.beginTransaction();
-
-		String consInsert = "insert into Departamentos (deptNo, dnombre, loc)"
-				+ " select n.deptNo, n.dnombre, n.loc from Nuevos n";
-
+		BigInteger codRes=(BigInteger) session.createQuery("select max(r.codreserva)+1 from T3reservas r",BigInteger.class).uniqueResult();
+		T3reservas r=new T3reservas();
+		r.setCodreserva(codRes);
+		r.setT3clientes(c);
+		r.setT3habitaciones(h);
+		r.setCamassupletorias(suple);
+		r.setFechaentrada(mifechaE);
+		r.setFechasalida(mifechaS);
+		r.setDescuento(BigInteger.valueOf(descuento));
+		
+		session.persist(r);
+		session.close();
+		
 	}
 
 }
---
+++ ExamenEmpresa/src/main/java/principal/PrincipalEmpre.java
@@ -0,0 +1,197 @@
+package principal;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+import java.util.Set;
+import java.util.logging.LogManager;
+import java.util.logging.Logger;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.query.Query;
+
+import datosEmpre.Departamentos;
+import datosEmpre.Empleados;
+import datosEmpre.Empresas;
+import datosEmpre.Oficios;
+
+
+public class PrincipalEmpre {
+	private static SessionFactory sesion;
+	public static void main(String[] args) {
+		LogManager.getLogManager().reset();
+		Logger globalLogger = Logger.getLogger(java.util.logging.Logger.GLOBAL_LOGGER_NAME);
+		globalLogger.setLevel(java.util.logging.Level.OFF);
+		sesion=Conexion.getSession();
+		
+		//ejercicio1(1);
+		//ejercicio2("hjfh","ddsgsd","jkdsghijk",1,11);
+		//ejercicio3();
+		ejercicio4(11,"fdgsdfg","dkhf","jhfsd",107,3);
+		
+		sesion.close();
+	
+
+	}
+	
+	private static void ejercicio4(int codDep, String nomDep, String direcDep, String locaDep, int codJefe, int codEmpresa) {
+		Session session = sesion.openSession();
+		Transaction tx = session.beginTransaction();
+		boolean err= false;
+		
+		Departamentos dep=(Departamentos)session.get(Departamentos.class, codDep);
+		if(dep==null) {
+			System.out.println("Departemento no existe");
+			err=true;
+		}
+		
+		Empleados jefe=(Empleados)session.get(Empleados.class,codJefe );
+		if(jefe==null) {
+			System.out.println("No existe Empleado");
+			err=true;
+		}
+		
+		Empresas e=(Empresas)session.get(Empresas.class, codEmpresa);
+		if(e==null) {
+			System.out.println("No existe cod empresa");
+			err=true;
+		}
+		
+		if(err) {
+			System.out.println("No se ha podido actualizar");
+			return;
+		}
+		
+		dep.setNombre(nomDep);
+		dep.setDireccion(direcDep);
+		dep.setLocalidad(locaDep);
+		dep.setEmpleados(jefe);
+		dep.setEmpresas(e);
+		
+		System.out.println("Se ha actualizado el departamento");
+		session.merge(dep);
+		tx.commit();
+		session.close();
+			
+	}
+
+	private static void ejercicio3() {
+		Session session = sesion.openSession();
+		Query cons=session.createQuery("from Oficios",Oficios.class);
+		int nEmpleados=0;
+		
+		List<Oficios> listaOfi=cons.getResultList();
+		System.out.println("Datos de Oficios");
+		System.out.printf("%10s %-30s %10s %16s %14s %10s%n","Codigo","Nombre","Salario Mes","Numero Empleados","Precio Trienio","Total Salario");
+		System.out.printf("%10s %-30s %10s %16s %14s %10s%n","----------","------------------------------","----------","----------------","--------------","----------");
+		for (Oficios ofi : listaOfi) {
+
+			nEmpleados=ofi.getEmpleadoses().size();
+			System.out.printf("%10s %-30s %10s %16s %14s %10s%n",ofi.getCodoficio(),ofi.getNombre(),ofi.getSalariomes(),nEmpleados,ofi.getPreciotrienio(),(ofi.getSalariomes()+ofi.getPreciotrienio()));
+			
+		}
+		
+	}
+	private static void ejercicio2(String nomEmple, String direc , String poblacion, long codOfi, long codDepart) {
+		Session session = sesion.openSession();
+		System.out.println("\nInsertar nuevos empleados\n----------------------------");
+		Transaction tx = session.beginTransaction();
+		boolean err=false;
+		
+		Oficios o=(Oficios)session.get(Oficios.class,codOfi);
+		if(o==null) {
+			System.out.println("Oficio no existe");
+			err=true;
+		}
+		
+		Departamentos d=(Departamentos)session.get(Departamentos.class,codDepart);
+		if(d==null) {
+			System.out.println("Departamento no existe");
+			err=true;
+		}
+		
+		if(err) {
+			System.out.println("No se ha insertado");
+			return;
+		}
+		
+		Empleados jefe=d.getEmpleados(); 
+		Date fechaActual=new Date();
+		
+		
+		
+		long codEmple=(long)session.createQuery("select max(e.codemple)+1 from Empleados e",long.class).uniqueResult();
+		
+		Empleados e=new Empleados();
+		e.setNombre(nomEmple);
+		e.setDireccion(direc);
+		e.setPoblacion(poblacion);
+		e.setOficios(o);
+		e.setDepartamentos(d);
+		e.setFechaalta(fechaActual);
+		e.setEmpleados(jefe);
+		e.setCodemple(codEmple);
+		
+		System.out.println("Se ha insertado");
+		
+		session.persist(e);
+		tx.commit();
+		session.close();
+	}
+	private static void ejercicio1(long codEmp) {
+		Session session = sesion.openSession();
+		Empresas e=(Empresas)session.get(Empresas.class,codEmp);
+		
+		if(e==null) {
+			System.out.println("No existe Empresa");
+			return;
+		}
+		
+		int nDpart=e.getDepartamentoses().size();
+
+		System.out.println("COD-EMPRESA: "+e.getCodempre()+" NOMBRE: "+e.getNombre());
+		System.out.println("DIRECCION: "+e.getDireccion()+" Numero de departamentos: "+nDpart);
+		System.out.println("-----------------------------------------------------------------------------------");
+		
+		Set<Departamentos> listDepart=e.getDepartamentoses();
+		for (Departamentos dep : listDepart) {
+			System.out.println("COD-DEPARTAMENTO: "+dep.getCoddepart()+" NOMBRE: "+dep.getNombre()+" LOCALIDAD: "+dep.getLocalidad());
+			
+			if(dep.getEmpleadoses().size()==0) {
+				System.out.println("NO tiene empleados");
+				continue;
+			}
+			
+			System.out.printf("%12s %-35s %-30s %-35s%n","COD-EMPLEADO","NOMBRE","OFICIO","NOMBRE ENCARGADO");
+			System.out.printf("%12s %-35s %-30s %-35s%n","------------","-----------------------------------","------------------------------","-----------------------------------");
+			
+			Set<Empleados> listEmple=dep.getEmpleadoses();
+			for (Empleados emp : listEmple) {
+				String nomEncargado="";
+				
+				if(emp.getEmpleados()==null) {
+					nomEncargado="No tiene encargado";
+				}else {
+					nomEncargado=emp.getEmpleados().getNombre();
+				}
+				
+//				try {
+//					nomEncargado=emp.getEmpleados().getNombre();
+//				} catch (Exception e2) {
+//					nomEncargado="No tiene encargado";
+//				}
+				System.out.printf("%12s %-35s %-30s %-35s%n",emp.getCodemple(),emp.getNombre(),emp.getOficios().getNombre(),nomEncargado);
+			}
+			
+			System.out.printf("%12s %-35s %-30s %-35s%n","------------","-----------------------------------","------------------------------","-----------------------------------");
+			System.out.println("Numero de empleados por departamento: "+dep.getEmpleadoses().size());
+			System.out.println("Nombre del jefe del departamento: "+dep.getEmpleados().getNombre());
+			
+		}
+		
+	}
+
+}
